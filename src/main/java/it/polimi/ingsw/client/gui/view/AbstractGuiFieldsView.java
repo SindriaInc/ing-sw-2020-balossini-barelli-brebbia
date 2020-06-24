472bb0beac06a42231f0d2683bcaa4814da7eb6b
MarcoBalossini <10578347@polimi.it>
2020-06-25 01:48:36 +0200

    Feat: Implement gui end view style, Restyle input button

--- src/main/java/it/polimi/ingsw/client/gui/view/AbstractGuiFieldsView.java
+++ src/main/java/it/polimi/ingsw/client/gui/view/AbstractGuiFieldsView.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.client.gui.view;
 
 import it.polimi.ingsw.client.gui.GuiAssets;
+import it.polimi.ingsw.client.gui.GuiConstants;
 import it.polimi.ingsw.client.gui.view.component.IntegerField;
 import it.polimi.ingsw.client.gui.view.presentation.FieldsPresentation;
 import javafx.beans.property.ReadOnlyDoubleProperty;
@@ -9,9 +10,13 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Stack;
 
 public abstract class AbstractGuiFieldsView extends AbstractGuiView {
 
@@ -41,10 +46,14 @@ public abstract class AbstractGuiFieldsView extends AbstractGuiView {
         IntegerField integer = new IntegerField();
         integer.setPromptText(integerPrompt);
 
-        Button connect = new Button();
-        connect.setText(buttonText);
+        Button button = new Button();
+        button.setText(buttonText);
+        ImageView buttonImage = new ImageView(getAssets().getImage(GuiAssets.Images.BUTTON_MAIN));
+
+        StackPane connect = new StackPane(buttonImage, button);
         connect.disableProperty().bind(integer.validBinding().not());
-        connect.setOnAction(event -> onAction(string.getText(), integer.getValue().orElse(0)));
+        button.setOnAction(event -> onAction(string.getText(), integer.getValue().orElse(0)));
+        buttonImage.setOnMouseClicked(event -> onAction(string.getText(), integer.getValue().orElse(0)));
 
         if (lastMessage != null) {
             if (currentAlert != null) {
@@ -60,7 +69,7 @@ public abstract class AbstractGuiFieldsView extends AbstractGuiView {
         Map<Label, TextField> inputs = new LinkedHashMap<>();
         inputs.put(stringLabel, string);
         inputs.put(integerLabel, integer);
-        return presentation.generatePresentation(width, height, inputs, connect);
+        return presentation.generatePresentation(width, height, inputs, button, buttonImage, connect);
     }
 
     public abstract void onAction(String string, int integer);
--- src/main/java/it/polimi/ingsw/client/gui/view/presentation/AbstractPresentation.java
+++ src/main/java/it/polimi/ingsw/client/gui/view/presentation/AbstractPresentation.java
@@ -8,11 +8,9 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.Region;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
 public class AbstractPresentation {
@@ -90,9 +88,10 @@ public class AbstractPresentation {
 
     /**
      * Style a button
-     * @param component The field
+     * @param button The field
+     * @param buttonImage The field background
      */
-    public void style(Button component) {
+    public void style(ImageView buttonImage, Button button, StackPane pane) {
         CornerRadii corner = new CornerRadii(
                 GuiConstants.INPUT_CORNER_RADII,
                 GuiConstants.INPUT_CORNER_RADII,
@@ -101,16 +100,16 @@ public class AbstractPresentation {
                 false
         );
 
-        Background sleeping = new Background(new BackgroundFill(Color.WHITE, corner, Insets.EMPTY));
-        Background hover = new Background(new BackgroundFill(Color.rgb(220, 220, 255), corner, Insets.EMPTY));
-        Background click = new Background(new BackgroundFill(Color.rgb(204, 204, 255), corner, Insets.EMPTY));
+        button.setBackground(null);
+        button.setFont(getAssets().getInputButtonFont());
 
-        component.setMinHeight(GuiConstants.INPUT_HEIGHT);
-        component.setBackground(sleeping);
-        component.setOnMouseEntered(event -> component.setBackground(hover));
-        component.setOnMouseExited(event -> component.setBackground(sleeping));
-        component.setOnMousePressed(event -> component.setBackground(click));
-        component.setFont(assets.getFont());
+        buttonImage.setPreserveRatio(true);
+        buttonImage.setFitHeight(GuiConstants.INPUT_BUTTON_HEIGHT);
+
+        ColorAdjust colorAdjust = new ColorAdjust();
+        colorAdjust.setBrightness(+0.25);
+        pane.setOnMouseEntered(event -> buttonImage.setEffect(colorAdjust));
+        pane.setOnMouseExited(event -> buttonImage.setEffect(null));
     }
 
 }
