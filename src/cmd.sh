bc752552f127868b2e4f8cb5012548abc2e6a4cf
lucapitzoi <luca.pitzoi@sindria.org>
2021-02-26 21:51:30 +0100

    Added cmd.sh for custom CMD, Fix production deployment

---
+++ src/cmd.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+CONTAINER_GATEWAY=`/sbin/ip route|awk '/default/ { print $3 }'`
+
+# Adding host machine hostname
+echo -e "\n# Hostname for gateway" >> /etc/hosts
+echo -e "${CONTAINER_GATEWAY}\tdocker.host.internal\n" >> /etc/hosts
+
+#Override timezone by env
+if [ "$TZ" != "" ] || [ "$TZ" != "Europe/Rome" ]; then
+    ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+fi
+
+# App services
+screen -dm -S server java -jar /var/www/app/Santorini.jar server -port ${SANTORINI_SERVER_PORT}
